3a4eae08dd553792943f96aba7a6f195d859d529
markup54 <marzenakupka@tlen.pl>
2024-09-11 14:34:38 +0200

    wstawianie do tablicy liczb losowych i jej wypisywanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //losowanie liczb do tablicy 6 elementowej
+        /*
+        tablica musi mieć zdefiniowanyrozmiar,
+        którego potem nie można zmieniać
+        w Javie tablica może przechowywać typy proste i zlozone
+
+         */
+        int[] tablicaLiczbLosowych = new int[6];
+        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
+            tablicaLiczbLosowych[i] = (int) (Math.random()*100+1);//od 1 do 100
+        }
+
+        //wypisywanie tablicy na ekranie
+        System.out.println("Wylosowana tablica:");
+        for (int element:tablicaLiczbLosowych) {
+            //(typ nazwa:nazwakolekcji)
+            System.out.print(element+", ");
+        }
+
+
     }
 }
