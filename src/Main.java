b08d39488a9000a22724739a564714c354ba1658
markup54 <marzenakupka@tlen.pl>
2024-09-12 13:04:28 +0200

    klasa gra

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,14 @@ import java.util.*;
 
 public class Main {
 
-    private static ArrayList<Integer> wylosujListe(int ileElementow){
-        ArrayList<Integer> listaLiczbWylosowanychBezPowtorzen = new ArrayList<>();
-        for (int i = 0; i < ileElementow; i++) {
-            int liczba = (int)(Math.random()*100+1);
-            while (listaLiczbWylosowanychBezPowtorzen.contains(liczba)){
-                liczba = (int)(Math.random()*100+1);
-            }
-            listaLiczbWylosowanychBezPowtorzen.add(liczba);
-        }
-        return listaLiczbWylosowanychBezPowtorzen;
-    }
-    private static void wypiszListe(List<Integer> listaDoWypisania){
-        System.out.println("Wypisywana lista:");
-        for (int i = 0; i < listaDoWypisania.size(); i++) {
-            System.out.println(listaDoWypisania.get(i));
-        }
-    }
-
-    private static ArrayList<Integer> wstawLiczbyDoListy(int ileElementow){
-
-        ArrayList <Integer> listaLiczbZKlawiatury = new ArrayList<>();
-        Scanner klawiatura = new Scanner(System.in);
-        System.out.println("Podaj "+ileElementow+" liczb");
-        for (int i = 0; i < ileElementow; i++) {
-            int liczba = klawiatura.nextInt();
-            listaLiczbZKlawiatury.add(liczba);
-        }
-        return listaLiczbZKlawiatury;
-    }
-    private static LinkedList<Integer> zwrocElementyZObuList
-            (ArrayList<Integer>listaLiczbZKlawiatury,
-             ArrayList<Integer>listaLiczbWylosowanychBezPowtorzen ){
-        LinkedList<Integer> trafione = new LinkedList<>();
-        for (Integer wpisana:listaLiczbZKlawiatury) {
-            if(listaLiczbWylosowanychBezPowtorzen.contains(wpisana)){
-                trafione.add(wpisana);
-            }
-        }
-        return trafione;
-    }
 
     public static void main(String[] args) {
-        ArrayList<Integer> losowe = wylosujListe(6);
-        wypiszListe(losowe);
-        ArrayList<Integer> wpisane = wstawLiczbyDoListy(6);
-        wypiszListe(wpisane);
-        LinkedList<Integer> trafione =zwrocElementyZObuList(losowe,wpisane);
-        wypiszListe(trafione);
+        Gra gra1 = new Gra();
+        gra1.zagraj();
+        gra1.zagraj();
+
+        Gra gra2 = new Gra();
+        gra2.zagraj();
 
     }
 }
